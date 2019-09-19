8d842416fb7ec38b5d3c70b181717cbca01d73c6
sandicc <alexanderkoza97@gmail.com>
2019-09-19 16:03:46 +0200

    change error text

--- controllers/register.js
+++ controllers/register.js
@@ -24,7 +24,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         })
         .then(trx.commit)
         .catch(trx.rollback)
-    }).catch(err => res.status(400).json(err));
+    }).catch(err => res.status(400).json('there is some error'));
 }
 
 module.exports = {
